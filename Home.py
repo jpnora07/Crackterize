ec2c7520fcc268ef9a6c1e4bfdbc6a320e9b6ac1
Jayvee <lostjayvee456@gmail.com>
2023-02-04 17:27:51 +0800

    Jayvee's Edited

--- Home.py
+++ Home.py
@@ -4,7 +4,7 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QListView, QComboBox, QDialog, QVBoxLayout, QApplication
 
 
-
+#new
 class Ui_MainWindow(object):
 
     def closeIt(self):
