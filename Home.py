56c01d8e2999463e926b7cfd2af4a5d75a13d98e
jpnora07 <jpnora33@gmail.com>
2023-02-08 18:47:56 +0800

    Jp's Edited

--- Home.py
+++ Home.py
@@ -279,7 +279,9 @@ class Ui_MainWindow(object):
         delegate = AlignDelegate(self.howtoUse)
         self.howtoUse.setItemDelegate(delegate)
         self.howtoUse.setEditable(True)
-        self.howtoUse.lineEdit().setAlignment(QtCore.Qt.AlignCenter)
+        self.ledit = self.howtoUse.lineEdit()
+        self.ledit.setReadOnly(True)
+        self.ledit.setAlignment(Qt.AlignCenter)
         self.howtoUse.addItems(
             [
                 "STEP 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -304,11 +306,12 @@ class Ui_MainWindow(object):
         radius = 20
 
         # border - top - left - radius: {0}px;
-        # border - top - right - radius: {0}px;
+
         view.setStyleSheet(
             """
             background-color :rgba(255, 255, 255, 0.75);
             border-bottom-left-radius:{0}px;
+            border-top-right-radius: {0}px;
             border-bottom-right-radius:{0}px;
             """.format(radius)
         )
